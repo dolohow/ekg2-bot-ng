7fdfc4d2b70fbe0fa69da5b641cca9a75ae8e49c
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2013-01-15 21:31:51 +0100

    Fix payment check

--- bot.py
+++ bot.py
@@ -29,14 +29,14 @@ def textCommands(session, uid, type, text, sent_time, ignore_level):
         sendMessage.setUid(uid)
         sendMessage.setMsg('%.0f / %s GB, wykorzystano: %.0f%%' % (a[0], a[1], a[0]/a[1]*100))
         sendMessage.sendMessageByUid()
-    if text == '!acc load':
+    elif text == '!acc load':
         mysql.query('SELECT akt_down_lacza, akt_upl_lacza, load1, load2, load3 FROM serwery WHERE nazwa=%s', main.getServerNameFromUid(uid))
         a = mysql.fetchOne()
         sendMessage = main.SendMessage()
         sendMessage.setUid(uid)
         sendMessage.setMsg('DL: %.4s MB/s UL: %.4s MB/s   Load: %s %s %s' % (float(a[0])/1024, float(a[1])/1024, a[2], a[3], a[4]))
         sendMessage.sendMessageByUid()
-    if text == '!acc transfer':
+    elif text == '!acc transfer':
         mysql.query('SELECT pay_my, sposob_platnosci, id FROM users WHERE gg=%s', main.createGGNumberFromUid(uid))
         a = mysql.fetchOne()
         sendMessage = main.SendMessage()
@@ -44,20 +44,20 @@ def textCommands(session, uid, type, text, sent_time, ignore_level):
         if a[0] == 0:
             if a[1] == 'przelew':
                 sendMessage.setMsg('Ciekot Grzegorz\nAl. N.M.P.62/27\n42-200 Czestochowa\n\n36 1140 2004 0000 3902 3331 4150\n\ntytuł przelewu: Darowizna x%sx' % a[2])
-            if a[1] == 'paypal':
+            elif a[1] == 'paypal':
                 sendMessage.setMsg('paypal: woox@woox.pl\nproszę zaznaczyć prezent, krewni/znajomi')
-            if a[1] == 'przysluga':
+            elif a[1] == 'przysluga':
                 sendMessage.setMsg('Nie musisz nic płacić ;-)')
-        if a[0] == 1:
+        elif a[0] == 1:
             if a[1] == 'przelew':
                 random = ["wilk", "dudek", "puchacz", "żółw", "żaba", "jelonek", "jeż", "sum", "jaszczurka", "sokół", "nietoperz"]
                 sendMessage.setMsg('20 1140 2004 0000 3502 7432 5846\ntytuł przelewu: Allegro %s x%sx' % (choice(random), a[2]))
-            if a[1] == 'paypal':
+            elif a[1] == 'paypal':
                 sendMessage.setMsg('paypal: dolohow@gmail.com\nproszę zaznaczyć prezent, krewni/znajomi')
-        if a[0] == 2:
+        elif a[0] == 2:
             if a[1] == 'przelew':
                 sendMessage.setMsg('22 1140 2004 0000 3202 7438 8328\nDarowizna x%sx' % a[2])
-            if a[1] == 'paypal':
+            elif a[1] == 'paypal':
                 sendMessage.setMsg('paypal: reyderti@gmail.com\nproszę zaznaczyć prezent, krewni/znajomi')
         sendMessage.sendMessageByUid()
 
