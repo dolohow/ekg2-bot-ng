9ebbeebb17acaeb366a6f413df9cbfc834417b79
Łukasz Żarnowiecki <lukasz@zarnowiecki.pl>
2013-03-17 17:05:38 +0100

    New variable in regex

--- bot.py
+++ bot.py
@@ -9,6 +9,9 @@ from datetime import date
 import re
 import dddd as secret
 
+def obtainClearExpression(text):
+	return ' '.join(text.split())
+
 def regullarExpressions(uid, text):
 	checkItOut = text.decode('utf-8')
 	mysql = main.MySQLConnection('local')
@@ -29,7 +32,7 @@ def messagesForEveryone(uid, text):
 	mysql.query('SELECT command FROM botmsg')
 	data = mysql.fetchAll()
 	for i in data:
-		if text.strip() == i[0]:
+		if obtainClearExpression(text) == i[0]:
 			fetch = main.MySQLConnection('local')
 			fetch.query('SELECT msg FROM botmsg WHERE command=%s', i[0])
 			sendMessage = main.SendMessage()
@@ -41,7 +44,7 @@ def messagesForEveryone(uid, text):
 	del mysql
 
 def accountCommands(uid, text):
-	text = text.strip()
+	text = obtainClearExpression(text)
 	if main.checkUserExistence(uid):
 		mysql = main.MySQLConnection('remote')
 	if text == '!acc disk':
@@ -67,22 +70,22 @@ def accountCommands(uid, text):
 		sendMessage.setUid(uid)
 		if a[0] == 0:
 			if a[1] == 'przelew':
-			    sendMessage.setMsg('Ciekot Grzegorz\nAl. N.M.P.62/27\n42-200 Czestochowa\n\n36 1140 2004 0000 3902 3331 4150\n\ntytuł przelewu: Darowizna x%sx' % a[2])
+			    sendMessage.setMsg('Ciekot Grzegorz\nAl. N.M.P.62/27\n42-200 Czestochowa\n\n36 1140 2004 0000 3902 3331 4150\n\ntytuÅ przelewu: Darowizna x%sx' % a[2])
 			elif a[1] == 'paypal':
-			    sendMessage.setMsg('paypal: woox@woox.pl\nproszę zaznaczyć prezent, krewni/znajomi')
+			    sendMessage.setMsg('paypal: woox@woox.pl\nproszÄ zaznaczyÄ prezent, krewni/znajomi')
 			elif a[1] == 'przysluga':
-			    sendMessage.setMsg('Nie musisz nic płacić ;-)')
+			    sendMessage.setMsg('Nie musisz nic pÅaciÄ ;-)')
 		elif a[0] == 1:
 			if a[1] == 'przelew':
-			    random = ["wilk", "dudek", "puchacz", "żółw", "żaba", "jelonek", "jeż", "sum", "jaszczurka", "sokół", "nietoperz"]
-			    sendMessage.setMsg('20 1140 2004 0000 3502 7432 5846\ntytuł przelewu: Allegro %s x%sx' % (choice(random), a[2]))
+			    random = ["wilk", "dudek", "puchacz", "Å¼Ã³Åw", "Å¼aba", "jelonek", "jeÅ¼", "sum", "jaszczurka", "sokÃ³Å", "nietoperz"]
+			    sendMessage.setMsg('20 1140 2004 0000 3502 7432 5846\ntytuÅ przelewu: Allegro %s x%sx' % (choice(random), a[2]))
 			elif a[1] == 'paypal':
-			    sendMessage.setMsg('paypal: dolohow@gmail.com\nproszę zaznaczyć prezent, krewni/znajomi')
+			    sendMessage.setMsg('paypal: dolohow@gmail.com\nproszÄ zaznaczyÄ prezent, krewni/znajomi')
 		elif a[0] == 2:
 			if a[1] == 'przelew':
 			    sendMessage.setMsg('22 1140 2004 0000 3202 7438 8328\nDarowizna x%sx' % a[2])
 			elif a[1] == 'paypal':
-			    sendMessage.setMsg('paypal: reyderti@gmail.com\nproszę zaznaczyć prezent, krewni/znajomi')
+			    sendMessage.setMsg('paypal: reyderti@gmail.com\nproszÄ zaznaczyÄ prezent, krewni/znajomi')
 		sendMessage.sendMessageByUid()
 		return True
 	elif text == '!acc valid':
@@ -91,7 +94,7 @@ def accountCommands(uid, text):
 		a = mysql.fetchOne()
 		sendMessage = main.SendMessage()
 		sendMessage.setUid(uid)
-		sendMessage.setMsg('Ważność konta %s, pozostało %s dni' % (str(a[0]), str((a[0]-today).days)))
+		sendMessage.setMsg('WaÅ¼noÅÄ konta %s, pozostaÅo %s dni' % (str(a[0]), str((a[0]-today).days)))
 		sendMessage.sendMessageByUid()
 		return True
 	del mysql
@@ -102,7 +105,7 @@ def sshCommands(uid, text):
 		mysql.query('SELECT command FROM botssh')
 		data = mysql.fetchAll()
 	for i in data:
-		if text.strip() == i[0]:
+		if obtainClearExpression(text).find(i[0]) == 0:
 			fetch = main.MySQLConnection('local')
 			ssh = main.SSHConnection()
 			server = main.getServerNameFromUid(uid)
@@ -110,7 +113,7 @@ def sshCommands(uid, text):
 			fetch.query('SELECT execute FROM botssh WHERE command=%s', i[0])
 			execute = fetch.fetchOne()
 			s = Template(execute[0])
-			s = s.safe_substitute(nick=username[0])
+			s = s.safe_substitute(nick=username[0], name=re.split(' ', obtainClearExpression(text), 2)[2])
 			ssh.setConnection(server, 'root', secret.serversList[server])
 			ssh.setCommand(s)
 			sendMessage = main.SendMessage()
@@ -122,9 +125,10 @@ def sshCommands(uid, text):
 	del mysql
 
 def messageHandler(session, uid, type, text, sent_time, ignore_level):
-	if regullarExpressions(uid, text) != True and text.strip().find('!') == 0:
+	if regullarExpressions(uid, text) != True and obtainClearExpression(text).find('!') == 0:
 		messagesForEveryone(uid, text)
 	 	sshCommands(uid, text)
 		accountCommands(uid, text)
 
 ekg.handler_bind('protocol-message-received', messageHandler)
+
